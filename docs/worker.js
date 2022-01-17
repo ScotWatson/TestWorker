c41bca3aaac19e750b882e092b7f7863cb3e41ea
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2022-01-16 19:10:07 -0500

    Update worker.js

--- docs/worker.js
+++ docs/worker.js
@@ -4,12 +4,14 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 */
 
 self.importScripts("/WebInterface/worker_api.js");
+
+let idBtn;
 let promiseAddButton = objSystem.addButton();
-/*
 promiseAddButton.then(function (response) {
-  console.log(response);
+  idBtn = response.id;
 });
 promiseAddButton.catch(function (error) {
-  console.error(error);
+  objSystem.sendResponse({
+    console: error,
+  });
 });
-*/
